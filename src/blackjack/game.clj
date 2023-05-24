286809bb09baa8f2a477e1ed760d883202c9a55d
victorglim <victor.guedes888@gmail.com>
2023-05-23 21:44:27 -0300

    feat: building the logic that performs the validation by mapping the cards and transforming them from numbers to their respective values

--- src/blackjack/game.clj
+++ src/blackjack/game.clj
@@ -3,3 +3,16 @@
 (defn new-card []
   "Gerando cartas com números entre 1 à 13"
   (inc (rand-int 13)))
+
+(defn JQK->10 [card]
+  (if (> card 10) 10 card))
+
+(defn A->11 [card]
+  (if (= card 1) 11 card))
+
+(defn points-cards [cards]
+  (let [cards-without-JQK (map JQK->10 cards)
+        cards-with-A11 (map A->11 cards-without-JQK)
+        points-with-A-1 (reduce + cards-without-JQK)
+        points-with-A-11 (reduce + cards-with-A11)]
+    (if (> points-with-A-11 21) points-with-A-1 points-with-A-11)))
